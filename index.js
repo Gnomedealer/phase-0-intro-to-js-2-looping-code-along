bc0f6b58118b2f4039270c88a3a1f746e003e0e5
Tommy Nguyen <sixty22tommy@gmail.com>
2022-06-29 15:41:26 -0700

    commit tree ed43d2127b42bc30d12b1a7897f630ea711e1774

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cards = ["Tommy", "Melissa", "Elijah"]
+
+function writeCards(cards, event){
+    let cart = []
+    for (let i = 0; i < cards.length; i++) {
+        cart.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return cart
+}
+
+function countDown(){
+    let countDown = 10
+    while (countDown > -1){
+        console.log(countDown--)
+    }
+}
